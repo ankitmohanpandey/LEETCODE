4adaddc917128aa225e26833443db78428c6a23a
Ankit Mohan Pandey <89546350+ankitmohanpandey@users.noreply.github.com>
2026-06-28 00:35:02 +0530

    Time: 7 ms (52.52%) | Memory: 23.4 MB (22.57%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -10,11 +10,8 @@ class Solution:
                 left += 1
                 right -= 1
 
-        # Step 1
         reverse(0, n - 1)
 
-        # Step 2
         reverse(0, k - 1)
 
-        # Step 3
         reverse(k, n - 1)
